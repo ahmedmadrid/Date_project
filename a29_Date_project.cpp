7df1a62202dff212c105cded09847c293a0decf0
Ahmed Basiony <92553822+ahmedmadrid@users.noreply.github.com>
2023-06-18 00:10:35 +0200

    Date_project

---
+++ a29_Date_project.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "clsDate.h"
+using namespace std;
+
+
+int main()
+{
+    
+    
+   
+
+   
+
+    /*clsDate date2("200/5/2023");
+    date2.printDate();*/
+
+
+    clsDate date4(255, 2024);
+    date4.printYearCalendar();
+
+    /*clsDate date1("31/11/2020");
+    date1.dateAfterAddingOneDay();
+    date1.printDate();*/
+
+
+    return 0;
+}
+
